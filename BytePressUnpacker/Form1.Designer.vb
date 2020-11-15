a00c508a3ce6ef26586bba7e58937b809734b0a9
misonothx but as garfield <61099345+miso-xyz@users.noreply.github.com>
2020-11-15 18:12:54 +0000

    Add files via upload

---
+++ BytePressUnpacker/Form1.Designer.vb
@@ -0,0 +1,321 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.CheckBox2 = New System.Windows.Forms.CheckBox()
+        Me.CheckBox1 = New System.Windows.Forms.CheckBox()
+        Me.SplitContainer1 = New System.Windows.Forms.SplitContainer()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.RichTextBox1 = New System.Windows.Forms.RichTextBox()
+        Me.Panel2 = New System.Windows.Forms.Panel()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.OpenFileDialog1 = New System.Windows.Forms.OpenFileDialog()
+        Me.ContextMenu1 = New System.Windows.Forms.ContextMenu()
+        Me.MenuItem1 = New System.Windows.Forms.MenuItem()
+        Me.MenuItem2 = New System.Windows.Forms.MenuItem()
+        Me.MenuItem3 = New System.Windows.Forms.MenuItem()
+        Me.MenuItem4 = New System.Windows.Forms.MenuItem()
+        Me.MenuItem5 = New System.Windows.Forms.MenuItem()
+        Me.MenuItem6 = New System.Windows.Forms.MenuItem()
+        Me.Panel1.SuspendLayout()
+        CType(Me.SplitContainer1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SplitContainer1.Panel1.SuspendLayout()
+        Me.SplitContainer1.Panel2.SuspendLayout()
+        Me.SplitContainer1.SuspendLayout()
+        Me.Panel2.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'Panel1
+        '
+        Me.Panel1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.Panel1.Controls.Add(Me.CheckBox2)
+        Me.Panel1.Controls.Add(Me.CheckBox1)
+        Me.Panel1.Dock = System.Windows.Forms.DockStyle.Top
+        Me.Panel1.Location = New System.Drawing.Point(0, 0)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(494, 32)
+        Me.Panel1.TabIndex = 1
+        '
+        'CheckBox2
+        '
+        Me.CheckBox2.AutoSize = True
+        Me.CheckBox2.Checked = True
+        Me.CheckBox2.CheckState = System.Windows.Forms.CheckState.Checked
+        Me.CheckBox2.Dock = System.Windows.Forms.DockStyle.Left
+        Me.CheckBox2.Location = New System.Drawing.Point(92, 0)
+        Me.CheckBox2.Name = "CheckBox2"
+        Me.CheckBox2.Size = New System.Drawing.Size(165, 28)
+        Me.CheckBox2.TabIndex = 1
+        Me.CheckBox2.Text = "Also extract ""bytepress.lib.dll"""
+        Me.CheckBox2.UseVisualStyleBackColor = True
+        '
+        'CheckBox1
+        '
+        Me.CheckBox1.AutoSize = True
+        Me.CheckBox1.Checked = True
+        Me.CheckBox1.CheckState = System.Windows.Forms.CheckState.Checked
+        Me.CheckBox1.Dock = System.Windows.Forms.DockStyle.Left
+        Me.CheckBox1.Location = New System.Drawing.Point(0, 0)
+        Me.CheckBox1.Name = "CheckBox1"
+        Me.CheckBox1.Size = New System.Drawing.Size(92, 28)
+        Me.CheckBox1.TabIndex = 0
+        Me.CheckBox1.Text = "AlwaysOnTop"
+        Me.CheckBox1.UseVisualStyleBackColor = True
+        '
+        'SplitContainer1
+        '
+        Me.SplitContainer1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.SplitContainer1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.SplitContainer1.Enabled = False
+        Me.SplitContainer1.IsSplitterFixed = True
+        Me.SplitContainer1.Location = New System.Drawing.Point(0, 32)
+        Me.SplitContainer1.Name = "SplitContainer1"
+        '
+        'SplitContainer1.Panel1
+        '
+        Me.SplitContainer1.Panel1.Controls.Add(Me.Label4)
+        Me.SplitContainer1.Panel1.Controls.Add(Me.Button2)
+        Me.SplitContainer1.Panel1.Controls.Add(Me.Button1)
+        '
+        'SplitContainer1.Panel2
+        '
+        Me.SplitContainer1.Panel2.Controls.Add(Me.RichTextBox1)
+        Me.SplitContainer1.Size = New System.Drawing.Size(494, 232)
+        Me.SplitContainer1.SplitterDistance = 161
+        Me.SplitContainer1.TabIndex = 2
+        '
+        'Label4
+        '
+        Me.Label4.Dock = System.Windows.Forms.DockStyle.Bottom
+        Me.Label4.Location = New System.Drawing.Point(0, 215)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(157, 13)
+        Me.Label4.TabIndex = 2
+        Me.Label4.Text = "(Tested on Version 1.0.0.2)"
+        Me.Label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'Button2
+        '
+        Me.Button2.Dock = System.Windows.Forms.DockStyle.Top
+        Me.Button2.Location = New System.Drawing.Point(0, 22)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(157, 22)
+        Me.Button2.TabIndex = 1
+        Me.Button2.Text = "Extract Files"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button1
+        '
+        Me.Button1.Dock = System.Windows.Forms.DockStyle.Top
+        Me.Button1.Location = New System.Drawing.Point(0, 0)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(157, 22)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "List Resources"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'RichTextBox1
+        '
+        Me.RichTextBox1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.RichTextBox1.Font = New System.Drawing.Font("Consolas", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.RichTextBox1.Location = New System.Drawing.Point(0, 0)
+        Me.RichTextBox1.Name = "RichTextBox1"
+        Me.RichTextBox1.ReadOnly = True
+        Me.RichTextBox1.Size = New System.Drawing.Size(325, 228)
+        Me.RichTextBox1.TabIndex = 0
+        Me.RichTextBox1.Text = ""
+        '
+        'Panel2
+        '
+        Me.Panel2.AllowDrop = True
+        Me.Panel2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.Panel2.Controls.Add(Me.Label3)
+        Me.Panel2.Controls.Add(Me.Button4)
+        Me.Panel2.Controls.Add(Me.Button3)
+        Me.Panel2.Controls.Add(Me.Label2)
+        Me.Panel2.Controls.Add(Me.TextBox1)
+        Me.Panel2.Controls.Add(Me.Label1)
+        Me.Panel2.Location = New System.Drawing.Point(79, 89)
+        Me.Panel2.Name = "Panel2"
+        Me.Panel2.Size = New System.Drawing.Size(331, 83)
+        Me.Panel2.TabIndex = 3
+        '
+        'Label3
+        '
+        Me.Label3.Location = New System.Drawing.Point(124, 31)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(118, 23)
+        Me.Label3.TabIndex = 5
+        Me.Label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'Button4
+        '
+        Me.Button4.Location = New System.Drawing.Point(43, 31)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(75, 23)
+        Me.Button4.TabIndex = 4
+        Me.Button4.Text = "&Browse..."
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Enabled = False
+        Me.Button3.Location = New System.Drawing.Point(248, 31)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(75, 23)
+        Me.Button3.TabIndex = 3
+        Me.Button3.Text = "&Ok"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(63, 59)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(227, 13)
+        Me.Label2.TabIndex = 2
+        Me.Label2.Text = "(you can also drag && drop the packed file here)"
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(43, 5)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(280, 20)
+        Me.TextBox1.TabIndex = 1
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(5, 8)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(32, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Path:"
+        '
+        'OpenFileDialog1
+        '
+        Me.OpenFileDialog1.FileName = "OpenFileDialog1"
+        '
+        'ContextMenu1
+        '
+        Me.ContextMenu1.MenuItems.AddRange(New System.Windows.Forms.MenuItem() {Me.MenuItem1, Me.MenuItem2, Me.MenuItem3, Me.MenuItem4, Me.MenuItem5, Me.MenuItem6})
+        '
+        'MenuItem1
+        '
+        Me.MenuItem1.Checked = True
+        Me.MenuItem1.Enabled = False
+        Me.MenuItem1.Index = 0
+        Me.MenuItem1.RadioCheck = True
+        Me.MenuItem1.Text = "app by misonothx"
+        '
+        'MenuItem2
+        '
+        Me.MenuItem2.Index = 1
+        Me.MenuItem2.Text = "-"
+        '
+        'MenuItem3
+        '
+        Me.MenuItem3.Checked = True
+        Me.MenuItem3.Index = 2
+        Me.MenuItem3.Text = "Open BytePress Github Repository"
+        '
+        'MenuItem4
+        '
+        Me.MenuItem4.Checked = True
+        Me.MenuItem4.Index = 3
+        Me.MenuItem4.Text = "Open BytePress Unpacker Github Repository"
+        '
+        'MenuItem5
+        '
+        Me.MenuItem5.Index = 4
+        Me.MenuItem5.Text = "-"
+        '
+        'MenuItem6
+        '
+        Me.MenuItem6.Checked = True
+        Me.MenuItem6.Enabled = False
+        Me.MenuItem6.Index = 5
+        Me.MenuItem6.RadioCheck = True
+        Me.MenuItem6.Text = "sinister.ly <3"
+        '
+        'Form1
+        '
+        Me.AllowDrop = True
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(494, 264)
+        Me.Controls.Add(Me.Panel2)
+        Me.Controls.Add(Me.SplitContainer1)
+        Me.Controls.Add(Me.Panel1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog
+        Me.HelpButton = True
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "Form1"
+        Me.ShowIcon = False
+        Me.ShowInTaskbar = False
+        Me.Text = "BytePress Unpacker"
+        Me.Panel1.ResumeLayout(False)
+        Me.Panel1.PerformLayout()
+        Me.SplitContainer1.Panel1.ResumeLayout(False)
+        Me.SplitContainer1.Panel2.ResumeLayout(False)
+        CType(Me.SplitContainer1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.SplitContainer1.ResumeLayout(False)
+        Me.Panel2.ResumeLayout(False)
+        Me.Panel2.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents Panel1 As System.Windows.Forms.Panel
+    Friend WithEvents CheckBox1 As System.Windows.Forms.CheckBox
+    Friend WithEvents SplitContainer1 As System.Windows.Forms.SplitContainer
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents Button2 As System.Windows.Forms.Button
+    Friend WithEvents RichTextBox1 As System.Windows.Forms.RichTextBox
+    Friend WithEvents Panel2 As System.Windows.Forms.Panel
+    Friend WithEvents Button4 As System.Windows.Forms.Button
+    Friend WithEvents Button3 As System.Windows.Forms.Button
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents OpenFileDialog1 As System.Windows.Forms.OpenFileDialog
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents CheckBox2 As System.Windows.Forms.CheckBox
+    Friend WithEvents ContextMenu1 As System.Windows.Forms.ContextMenu
+    Friend WithEvents MenuItem1 As System.Windows.Forms.MenuItem
+    Friend WithEvents MenuItem2 As System.Windows.Forms.MenuItem
+    Friend WithEvents MenuItem3 As System.Windows.Forms.MenuItem
+    Friend WithEvents MenuItem4 As System.Windows.Forms.MenuItem
+    Friend WithEvents MenuItem5 As System.Windows.Forms.MenuItem
+    Friend WithEvents MenuItem6 As System.Windows.Forms.MenuItem
+
+End Class
---
+++ BytePressUnpacker/Form1.vb
@@ -0,0 +1,105 @@
+﻿Imports System.IO
+Imports System.Reflection
+Public Class Form1
+    Dim upk_dll As Assembly
+    Private Sub TextBox1_TextChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles TextBox1.TextChanged
+        Button3.Enabled = False
+        If IO.File.Exists(TextBox1.Text) Then
+            Try
+                System.Reflection.AssemblyName.GetAssemblyName(TextBox1.Text)
+            Catch ex As Exception
+                TextBox1.BackColor = ColorTranslator.FromHtml("#E9C083")
+                Label3.Text = "Not an assembly file."
+                Return
+            End Try
+            TextBox1.BackColor = ColorTranslator.FromHtml("#AAE895")
+            Label3.Text = "File is valid."
+            Button3.Enabled = True
+        Else
+            TextBox1.BackColor = ColorTranslator.FromHtml("#EB8181")
+            Label3.Text = "Invalid path."
+        End If
+    End Sub
+
+    Private Sub Button3_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button3.Click
+        Panel2.Hide()
+        SplitContainer1.Enabled = True
+    End Sub
+
+    Private Sub Button4_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button4.Click
+        If OpenFileDialog1.ShowDialog = vbOK Then
+            TextBox1.Text = OpenFileDialog1.FileName
+        End If
+    End Sub
+
+    Private Sub Panel2_DragDrop(ByVal sender As System.Object, ByVal e As System.Windows.Forms.DragEventArgs) Handles Panel2.DragDrop
+        Dim files() As String = e.Data.GetData(DataFormats.FileDrop)
+        TextBox1.Text = files(0)
+    End Sub
+
+    Private Sub Panel2_DragEnter(ByVal sender As System.Object, ByVal e As System.Windows.Forms.DragEventArgs) Handles Panel2.DragEnter
+        If e.Data.GetDataPresent(DataFormats.FileDrop) Then
+            e.Effect = DragDropEffects.Copy
+        End If
+    End Sub
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        RichTextBox1.Clear()
+        Dim asm As Assembly = Assembly.LoadFile(TextBox1.Text)
+        RichTextBox1.Text = "Resources of """ & IO.Path.GetFileName(TextBox1.Text) & """ (" & asm.GetManifestResourceNames.Count & ")" & vbCrLf & "- %Name% | %HEXSize%" & vbCrLf & vbCrLf
+        For x = 0 To asm.GetManifestResourceNames.Count - 1
+            RichTextBox1.Text += "- " & asm.GetManifestResourceNames(x) & " | 0x" & Hex(asm.GetManifestResourceStream(asm.GetManifestResourceNames(x)).Length.ToString) & vbCrLf
+        Next
+    End Sub
+    Function decompress(ByVal data As Byte(), ByVal compressionType As Integer)
+        Dim asm As Assembly = Assembly.LoadFile(TextBox1.Text)
+        Dim method As MethodInfo = upk_dll.GetType("bytepress.lib.Main").GetMethod("Decompress")
+        Return CType(method.Invoke(Nothing, New Object() {data, compressionType}), Byte())
+    End Function
+    Private Sub Button2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button2.Click
+        RichTextBox1.Clear()
+        IO.Directory.CreateDirectory("BytePress_Unpacker\" & IO.Path.GetFileNameWithoutExtension(TextBox1.Text))
+        Using manifestResourceStream As Stream = Assembly.LoadFile(TextBox1.Text).GetManifestResourceStream("bytepress.lib.dll")
+            Using memoryStream As MemoryStream = New MemoryStream()
+                manifestResourceStream.CopyTo(memoryStream)
+                upk_dll = Assembly.Load(memoryStream.ToArray())
+                If CheckBox2.Checked Then
+                    RichTextBox1.Text += "Extracting ""bytepress.lib.dll""..." & vbCrLf
+                    IO.File.WriteAllBytes("BytePress_Unpacker\" & IO.Path.GetFileNameWithoutExtension(TextBox1.Text) & "\bytepress.lib.dll", memoryStream.ToArray())
+                    RichTextBox1.Text += """bytepress.lib.dll"" successfully extracted!" & vbCrLf & vbCrLf
+                End If
+            End Using
+        End Using
+        Using manifestResourceStream2 As Stream = Assembly.LoadFile(TextBox1.Text).GetManifestResourceStream("data")
+            Using memoryStream2 As MemoryStream = New MemoryStream()
+                manifestResourceStream2.CopyTo(memoryStream2)
+                Dim array As Byte() = decompress(memoryStream2.ToArray(), 2)
+                If array Is Nothing OrElse array.Length = 0 Then
+                    Throw New Exception("Failed to decompress file")
+                End If
+                RichTextBox1.Text += "Extracting ""data.bin""..." & vbCrLf
+                IO.File.WriteAllBytes("BytePress_Unpacker\" & IO.Path.GetFileNameWithoutExtension(TextBox1.Text) & "\data.bin", array)
+                RichTextBox1.Text += """data.bin"" successfully extracted!"
+            End Using
+        End Using
+        If MessageBox.Show("Files have been extracted, do you want to open the output folder?", "BytePress Unpacker", MessageBoxButtons.YesNo, MessageBoxIcon.Information) = Windows.Forms.DialogResult.Yes Then
+            Process.Start("explorer.exe", "BytePress_Unpacker\" & IO.Path.GetFileNameWithoutExtension(TextBox1.Text))
+        End If
+    End Sub
+
+    Private Sub CheckBox1_CheckedChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles CheckBox1.CheckedChanged
+        TopMost = CheckBox1.Checked
+    End Sub
+
+    Private Sub MenuItem3_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MenuItem3.Click
+        Process.Start("https://github.com/roachadam/bytepress")
+    End Sub
+
+    Private Sub MenuItem4_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MenuItem4.Click
+        Process.Start("https://github.com/miso-xyz/BytePressUnpacker")
+    End Sub
+
+    Private Sub Form1_HelpButtonClicked(ByVal sender As System.Object, ByVal e As System.ComponentModel.CancelEventArgs) Handles MyBase.HelpButtonClicked
+        ContextMenu1.Show(Me, New Point(Size.Width - 80, 0))
+    End Sub
+End Class
